62f244f8542e191dfe47eac10924a530692dec7d
hassan <h.souiti@gmail.com>
2021-11-24 15:01:52 +0100

    start structuring the backemd...

---
+++ backend/configuration/config.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+const config = {
+    ENV = process.env.NODE_ENV || 'development',
+    PORT = process.env.PORT || '3500',
+    MONGO_URL = process.env.MONGO_URL
+}
+
+module.exports = config
