e9cc702465c22902b0f565b419dc386348e02915
Hassan <h.souiti@gmail.com>
2021-11-27 07:46:23 +0100

    Updating config && package.json...

--- backend/config/index.js
+++ backend/config/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const config = {
   ENV: process.env.NODE_ENV || "development",
-  PORT: process.env.PORT || "3500",
+  PORT: process.env.PORT || 3500,
   MONGO_URL: process.env.MONGO_URL,
 };
 
--- backend/configuration/config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-const config = {
-    ENV = process.env.NODE_ENV || 'development',
-    PORT = process.env.PORT || '3500',
-    MONGO_URL = process.env.MONGO_URL
-}
-
-module.exports = config
