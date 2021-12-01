6ccfb55e4d04bcdc5e16819c2b5d93e6150bd8e5
Hassan <h.souiti@gmail.com>
2021-12-01 06:34:51 +0100

    Adding users & tickets routes

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,16 @@ import express from 'express'
 const app = express()
 
 import projectsRouter from './routes/projects.js'
+import usersRouter from './routes/users.js'
+import ticketsRouter from './routes/tickets.js'
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.get('/', (req, res) => res.send('Welcome'))
 app.use('/api/v1/projects', projectsRouter)
+app.use('/api/v1/users', usersRouter)
+app.use('/api/v1/tickets', ticketsRouter)
 
 
 export default app
