c7d98d0186514ad08124338a2b6ac696e79ce481
Hassan <h.souiti@gmail.com>
2021-12-17 12:39:14 +0100

    intalling & using cookie-parser

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,33 @@
-import express from 'express'
+import dotenv from 'dotenv';
+dotenv.config();
 
-import errorsHandler from './middlewares/errorsHandler.js'
-import notFound from './middlewares/notFound.js'
+import express from 'express';
 
-import projectsRouter from './routes/projects.js'
-import usersRouter from './routes/users.js'
-import ticketsRouter from './routes/tickets.js'
+import cookieParser from 'cookie-parser';
+
+import errorsHandler from './middlewares/errorsHandler.js';
+import notFound from './middlewares/notFound.js';
+
+import projectsRouter from './routes/projects.js';
+import usersRouter from './routes/users.js';
+import ticketsRouter from './routes/tickets.js';
 import authRouter from './routes/auth.js';
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
 
 // Routes
 app.get('/', (req, res) => res.send('Welcome'));
 app.use('/api/v1/projects', projectsRouter);
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users', usersRouter)
-app.use('/api/v1/tickets', ticketsRouter)
+app.use('/api/v1/users', usersRouter);
+app.use('/api/v1/tickets', ticketsRouter);
 
 // Errors Handler
-app.use(notFound)
-app.use(errorsHandler)
+app.use(notFound);
+app.use(errorsHandler);
 
-
-export default app
+export default app;
---
+++ backend/middlewares/isAuthanticated.js
@@ -0,0 +1,3 @@
+import jwt from 'jsonwebtoken';
+
+export const isAuthenticated = (req, res, next) => {};
