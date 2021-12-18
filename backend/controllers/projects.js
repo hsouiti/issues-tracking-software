a8aa2d9f0596ca1a6d2349cf1cd5ce562f788658
Hassan <h.souiti@gmail.com>
2021-12-18 07:52:15 +0100

    refactoring code response

--- backend/controllers/projects.js
+++ backend/controllers/projects.js
@@ -1,7 +1,7 @@
-import Project from '../models/projects.js';
+import Project from '../models/projects.js'
 
-import APIError from '../errors/APIError.js';
-import HttpStatusCodes from '../errors/statusCodes.js';
+import APIError from '../errors/APIError.js'
+import HttpStatusCodes from '../errors/statusCodes.js'
 
 // get all the projects
 // TODO: Limit && sort projects
@@ -10,58 +10,65 @@ import HttpStatusCodes from '../errors/statusCodes.js';
 */
 
 export const getAllProjects = async (req, res, next) => {
-  const projects = await Project.find({}).sort({ createdAt: 'desc' });
-  res
-    .status(HttpStatusCodes.OK)
-    .json({ status: 'success', total: projects.length, data: projects });
-};
+  const projects = await Project.find({}).sort({ createdAt: 'desc' })
+  res.status(HttpStatusCodes.OK).json({
+    status: 'success',
+    data: {
+      total: projects.length,
+      projects,
+    },
+  })
+}
 
 // get a single project
 export const getProject = async (req, res, next) => {
-  const { id: projectID } = req.params;
+  const { id: projectID } = req.params
 
-  const project = await Project.findOne({ _id: projectID });
+  const project = await Project.findOne({ _id: projectID })
 
   if (!project) {
-    return next(APIError.HTTP400Error(`No Project with id: ${projectID}`));
+    return next(APIError.HTTP400Error(`No Project with id: ${projectID}`))
   }
 
-  res.status(HttpStatusCodes.OK).json({ status: 'success', data: project });
-};
+  res.status(HttpStatusCodes.OK).json({ status: 'success', data: project })
+}
 
 // create project
 export const createProject = async (req, res) => {
-  const project = await Project.create(req.body);
-  res
-    .status(HttpStatusCodes.CREATED)
-    .json({ status: 'success', data: project });
-};
+  const project = await Project.create(req.body)
+  res.status(HttpStatusCodes.CREATED).json({ status: 'success', data: project })
+}
 
 // update project
 export const updateProject = async (req, res) => {
-  const { id: projectID } = req.params;
+  const { id: projectID } = req.params
 
   const project = await Project.findOneAndUpdate({ _id: projectID }, req.body, {
     new: true,
     runValidators: true,
-  });
+  })
 
   if (!project) {
-    return next(APIError.HTTP400Error(`No Project with id: ${projectID}`));
+    return next(APIError.HTTP400Error(`No Project with id: ${projectID}`))
   }
 
-  res.status(202).json({ status: 'success', data: project });
-};
+  res.status(202).json({ status: 'success', data: project })
+}
 
 // delete project
 export const deleteProject = async (req, res, next) => {
-  const { id: projectID } = req.params;
+  const { id: projectID } = req.params
 
-  const project = await Project.findOneAndDelete({ _id: projectID });
+  const project = await Project.findOneAndDelete({ _id: projectID })
 
   if (!project) {
-    return next(APIError.HTTP400Error(`No Project with id: ${projectID}`));
+    return next(APIError.HTTP400Error(`No Project with id: ${projectID}`))
   }
 
-  res.status(202).json({ status: 'success', data: project });
-};
+  res
+    .status(202)
+    .json({
+      status: 'success',
+      data: { message: 'Project deleted succefully' },
+    })
+}
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,9 +1,9 @@
 import User from '../models/users.js'
 import mongoose from 'mongoose'
 import APIError from '../errors/APIError.js'
-import HttpStatusCodes from '../errors/statusCodes.js';
+import HttpStatusCodes from '../errors/statusCodes.js'
 
-const { ObjectId } = mongoose.Types;
+const { ObjectId } = mongoose.Types
 
 // get all the users
 // TODO: Limit && sort users
@@ -13,56 +13,66 @@ const { ObjectId } = mongoose.Types;
 export const getAllUsers = async (req, res, next) => {
   const users = await User.find({})
     .sort({ createdAt: 'desc' })
-    .select('-password');
-  res.status(HttpStatusCodes.OK).json({ total: users.length, users });
-};
+    .select('-password')
+
+  res.status(HttpStatusCodes.OK).json({
+    status: 'success',
+    data: {
+      total: users.length,
+      users,
+    },
+  })
+}
 
 // get a single user
 export const getUser = async (req, res, next) => {
-  const { id: userID } = req.params;
+  const { id: userID } = req.params
 
-  const user = await User.findOne({ _id: userID }).select('-password');
+  const user = await User.findOne({ _id: userID }).select('-password')
 
   if (!user) {
-    return next(APIError.HTTP400Error(`No User with id: ${userID}`));
+    return next(APIError.HTTP400Error(`No User with id: ${userID}`))
   }
 
-  res.status(HttpStatusCodes.OK).json({ user });
-};
+  res.status(HttpStatusCodes.OK).json({ status: 'success', data: user })
+}
 
 // create user
 export const createUser = async (req, res, next) => {
-  const user = await User.create(req.body);
-  res.status(HttpStatusCodes.CREATED).json({ user });
-};
+  const user = await User.create(req.body)
+  res.status(HttpStatusCodes.CREATED).json({ user })
+}
 
 // TODO: update user without password
 export const updateUser = async (req, res, next) => {
-  const { id: userID } = req.params;
+  const { id: userID } = req.params
 
   const user = await User.findOneAndUpdate({ _id: userID }, req.body, {
     new: true,
     runValidators: true,
-  });
+  }).select('-password')
 
   if (!user) {
-    return next(APIError.HTTP400Error(`No User with id: ${userID}`));
+    return next(APIError.HTTP400Error(`No User with id: ${userID}`))
   }
 
-  res.status(HttpStatusCodes.ACCEPTED).json({ user });
-};
+  res.status(HttpStatusCodes.ACCEPTED).json({ status: 'success', data: user })
+}
 
 // TODO: update password
 
 // delete user
 export const deleteUser = async (req, res, next) => {
-  const { id: userID } = req.params;
+  const { id: userID } = req.params
 
-  const user = await User.findOneAndDelete({ _id: userID });
+  const user = await User.findOneAndDelete({ _id: userID })
 
   if (!user) {
-    return next(APIError.HTTP400Error(`No User with id: ${userID}`));
+    return next(APIError.HTTP400Error(`No User with id: ${userID}`))
   }
 
-  res.status(HttpStatusCodes.ACCEPTED).json({ user });
-};
+  res.status(HttpStatusCodes.ACCEPTED).json({
+    status: 'success',
+    data: { message: ' User deleted succefully' },
+  })
+}
