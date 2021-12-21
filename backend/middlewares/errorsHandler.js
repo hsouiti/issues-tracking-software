d28baaa993e11b765eefce496a652e213b75e37b
Hassan <h.souiti@gmail.com>
2021-12-21 07:52:21 +0100

    create isAuthorized middelware

--- backend/middlewares/errorsHandler.js
+++ backend/middlewares/errorsHandler.js
@@ -1,12 +1,12 @@
 import HttpStatusCodes from '../errors/statusCodes.js';
 
 const errorsHanlder = (err, req, res, next) => {
-  //console.log(err);
+  //console.log(err)
 
   let showError = {
     statusCode: err.statusCode || HttpStatusCodes.INTERNAL_SERVER_ERROR,
     message: err.message || 'Something went wrong try again later',
-  };
+  }
 
   // monoose schema validation errors
   if (
@@ -15,29 +15,29 @@ const errorsHanlder = (err, req, res, next) => {
   ) {
     showError.message = Object.values(err.errors)
       .map((error) => error.properties.message)
-      .join(', ');
-    showError.statusCode = HttpStatusCodes.BAD_REQUEST;
+      .join(', ')
+    showError.statusCode = HttpStatusCodes.BAD_REQUEST
   }
 
   // Duplicate Key (unique)
   if (err.code && err.code === 11000) {
-    showError.statusCode = HttpStatusCodes.BAD_REQUEST;
+    showError.statusCode = HttpStatusCodes.BAD_REQUEST
     showError.message = `${Object.values(
       err.keyValue
-    )} is already exist, please choose another one`;
+    )} is already exist, please choose another one`
   }
 
   // CastError
   if (err.name === 'CastError') {
-    showError.message = `No item found with id: ${err.value}`;
-    showError.statusCode = HttpStatusCodes.BAD_REQUEST;
+    showError.message = `No item found with id: ${err.value}`
+    showError.statusCode = HttpStatusCodes.BAD_REQUEST
   }
 
   return res.status(showError.statusCode).json({
     status: 'error',
     statusCode: showError.statusCode,
     message: showError.message,
-  });
+  })
 };
 
 export default errorsHanlder;
--- backend/middlewares/isAuthorized.js
+++ backend/middlewares/isAuthorized.js
@@ -1,5 +1,17 @@
-const isAuthorized = async () => {
-  return;
-};
+import APIError from '../errors/APIError.js'
 
-export default isAuthorized;
+// TODO: for the roles developer & submitter
+//        check if he has the permissions to see (his profile)
+
+const isAuthorized = (...roles) => {
+  return (req, res, next) => {
+    console.log('roles', roles, req.user)
+    if (!roles.includes(req.user.role)) {
+      return next(APIError.Forbidden("you don't have access authorization"))
+    }
+    next()
+  }
+}
+
+export default isAuthorized
+
