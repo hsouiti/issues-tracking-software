fea70b1555f99d3b9578b2043a80d58c8c0f49ca
Hassan <h.souiti@gmail.com>
2022-09-07 15:29:34 +0100

    extends Request type express

--- api/global.d.ts
+++ api/global.d.ts
@@ -1,15 +1,24 @@
 /* eslint-disable prettier/prettier */
-export { }
+import { UserInput } from "./src/interfaces";
+
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            ENV: string;
+            NODE_ENV: 'development' | 'production';
             PORT: number;
-            MONGO_URL: string;
+            MONGODB_URI?: string;
             JWT_TOKEN_SECRET: string;
             JWT_TOKEN_TIME: string;
             PARSER_SECRET: string;
             STATIC_FOLDER: string;
         }
     }
+    // Extend Express Request
+    namespace Express {
+        interface Request {
+            user?: UserInput | undefined
+        }
+    }
 }
+
+export { }
