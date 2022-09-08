80c3b58274959661735bdc765d359acc3030ff18
Hassan <h.souiti@gmail.com>
2022-09-08 20:59:33 +0100

    Adding tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
+import './index.css';
 function App() {
-  return <div className="App">Hello World!</div>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
