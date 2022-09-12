2c3a633fc3e26bb20bd924319e2b913e40d9ef28
Hassan <h.souiti@gmail.com>
2022-09-12 16:50:55 +0100

    adding config

--- api/config/index.ts
+++ api/config/index.ts
@@ -7,6 +7,7 @@ const config = {
   NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT,
   MONGODB_URI: process.env.MONGODB_URI,
+  MONGODB_URI_TEST: process.env.MONGODB_URI_TEST,
   JWT_TOKEN_SECRET: process.env.JWT_TOKEN_SECRET,
   JWT_TOKEN_TIME: process.env.JWT_TOKEN_TIME,
   PARSER_SECRET: process.env.PARSER_SECRET,
