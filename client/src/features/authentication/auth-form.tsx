dadcdaecd9831eba398b9e71f369b01a5eaebcbe
Hassan <h.souiti@gmail.com>
2022-09-20 14:21:21 +0100

    create login form

--- client/src/features/authentication/auth-form.tsx
+++ client/src/features/authentication/auth-form.tsx
@@ -1,3 +1,36 @@
+import {useState} from 'react';
+
 export const AuthForm = () => {
-  return <div>auth-form</div>;
+  const [message, setMessage] = useState('message');
+  const handleClick = () => {
+    setMessage('submit success');
+  };
+
+  return (
+    <div>
+      <form>
+        <div data-testid="message">{message}</div>
+        <div className="inputs">
+          <div>
+            <label>Username</label>
+            <input type="text" className="border" name="username" />
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              type="password"
+              className="border"
+              name="password"
+              role="password"
+              aria-label="password"
+            />
+          </div>
+        </div>
+        <button disabled onClick={handleClick}>
+          Sign In
+        </button>
+        <button>Cancel </button>
+      </form>
+    </div>
+  );
 };
