37719373099b204f4b482f79275a493461531692
Hassan <h.souiti@gmail.com>
2022-09-21 10:27:00 +0100

    configure project

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,57 +4,17 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig(
-  {
-    resolve:
-      {
-        alias:
-          {
-            '@': path.resolve(
-              __dirname,
-              'src'
-            ),
-            '@assets':
-              path.resolve(
-                __dirname,
-                'src',
-                'assets'
-              ),
-            '@components':
-              path.resolve(
-                __dirname,
-                'src',
-                'components'
-              ),
-            '@layouts':
-              path.resolve(
-                __dirname,
-                'src',
-                'layouts'
-              ),
-            '@pages':
-              path.resolve(
-                __dirname,
-                'src',
-                'pages'
-              ),
-            '@features':
-              path.resolve(
-                __dirname,
-                'src',
-                'features'
-              ),
-            '@styles':
-              path.resolve(
-                __dirname,
-                'src',
-                'styles'
-              ),
-          },
-      },
-    plugins:
-      [
-        react(),
-      ],
-  }
-);
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src', 'assets'),
+      '@components': path.resolve(__dirname, 'src', 'components'),
+      '@layouts': path.resolve(__dirname, 'src', 'layouts'),
+      '@pages': path.resolve(__dirname, 'src', 'pages'),
+      '@features': path.resolve(__dirname, 'src', 'features'),
+      '@styles': path.resolve(__dirname, 'src', 'styles'),
+    },
+  },
+  plugins: [react()],
+});
