bbd8cd469e896e212f92317a1ea509da0e5d938a
Hassan <h.souiti@gmail.com>
2022-09-27 13:09:33 +0100

    finsihing useAuth

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -1,8 +1,26 @@
 import {useMemo} from 'react';
+import {Navigate} from 'react-router-dom';
 import {useSelector} from 'react-redux';
-import {getCurrentUser} from '../features/auth/service/authSlice';
+import {getCurrentToken} from '../features/auth/service/authSlice';
 
 export const useAuth = () => {
-  const user = useSelector(getCurrentUser);
-  return useMemo(() => ({user}), [user]);
+  function parseJwt(token: string) {
+    try {
+      return JSON.parse(atob(token.split('.')[1]));
+    } catch (e) {
+      return null;
+    }
+  }
+
+  const isValidToken = () => {
+    const tokenString = localStorage.getItem('access_token');
+    const decodedJwt = tokenString ? parseJwt(tokenString) : null;
+
+    if (decodedJwt) {
+      return Math.floor(new Date().getTime()) < decodedJwt.exp * 1000;
+    } else {
+      return false;
+    }
+  };
+  return isValidToken();
 };
