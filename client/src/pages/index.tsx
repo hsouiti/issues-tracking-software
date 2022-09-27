7cea730114e30b166311546dd6efde4e2caca728
Hassan <h.souiti@gmail.com>
2022-09-27 13:09:56 +0100

    index cleaning

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,20 +3,12 @@ import {useAuth} from '@/hooks/useAuth';
 import {SideBar} from '@layouts/side-bar';
 
 export const Index = () => {
-  const isAuthenticated = true;
-  const {user} = useAuth();
-  console.log('user', user);
-
-  if (user) {
-    return (
-      <div className="wrapper flex w-full min-h-screen bg-white border">
-        <SideBar />
-        <main className="main w-full border p-4">
-          <Outlet />
-        </main>
-      </div>
-    );
-  } else {
-    return <Navigate to="/login" />;
-  }
+  return (
+    <div className="wrapper flex w-full min-h-screen bg-white border">
+      <SideBar />
+      <main className="main w-full border p-4">
+        <Outlet />
+      </main>
+    </div>
+  );
 };
