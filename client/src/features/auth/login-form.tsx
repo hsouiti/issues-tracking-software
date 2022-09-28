f2c7e0a52c11e84782a2ded657802b4c9887bab8
Hassan <h.souiti@gmail.com>
2022-09-28 17:51:41 +0100

    cleaning the code

--- client/src/features/auth/login-form.tsx
+++ client/src/features/auth/login-form.tsx
@@ -1,89 +1,70 @@
-/* eslint-disable max-len */
-import {useEffect, useState} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {useNavigate} from 'react-router-dom';
+import {useRef} from 'react';
+import {useNavigate, Form} from 'react-router-dom';
+
+import {useLogUserMutation} from './api/authApi';
 
 import {ErrorType} from '../../types';
 
-import {useLogUserMutation} from './api/authApi';
-import {getCurrentUser} from './service/authSlice';
-//import {setUser} from './service/authSlice';
-
-const initialState = {
-  email: '',
-  password: '',
-};
-
 export const LoginForm = () => {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [values, setValues] = useState(initialState);
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {name, value} = e.target;
 
-    setValues({
-      ...values,
-      [name]: value,
-    });
-  };
+  const formRef = useRef<HTMLFormElement>(null);
 
   const [logUser, {isError, error: fetchError}] = useLogUserMutation();
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    signIn();
-  };
+  const submitForm = async () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-ignore
+    const formData = new FormData(formRef.current);
+    //formData.append('email', values.email);
+    //formData.append('password', values.password);
+    const updates = Object.fromEntries(formData);
 
-  const signIn = async () => {
     try {
-      const {email, password} = values;
-      const user = await logUser({email, password}).unwrap();
-      //setUser(user);
-
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore
+      const user = await logUser(updates).unwrap();
       navigate('/');
     } catch (error: unknown) {
       const err = error as ErrorType;
-      //console.log('aysnc error', err.data.message);
       console.log('aysnc error', err);
     }
+    navigate('/');
   };
 
-  useEffect(() => {
-    if (isError) console.log('fetcherror', fetchError);
-  }, [isError]);
-
   return (
-    <>
-      <div>
-        <form className="max-w-[400px] w-full mx-auto bg-white p-8" onSubmit={handleSubmit}>
-          <div className="inputs">
-            <div>
-              <label>Username</label>
-              <input
-                type="text"
-                className="border"
-                name="email"
-                value={values.email}
-                onChange={handleChange}
-              />
-            </div>
-            <div>
-              <label>Password</label>
-              <input
-                type="password"
-                className="border"
-                name="password"
-                role="password"
-                aria-label="password"
-                value={values.password}
-                onChange={handleChange}
-              />
-            </div>
+    <div>
+      <Form className="max-w-[400px] w-full mx-auto bg-white p-8" method="post" ref={formRef}>
+        <div className="inputs">
+          <div>
+            <label>Username</label>
+            <input
+              type="text"
+              className="border"
+              name="email"
+              aria-label="email"
+              /*   value={values.email}
+                onChange={handleChange} */
+            />
           </div>
-          <button style={{padding: '10px 20px', border: '1px solid gray'}}>Sign In</button>
-          <button>Cancel </button>
-        </form>
-      </div>
-    </>
+          <div>
+            <label>Password</label>
+            <input
+              type="password"
+              className="border"
+              name="password"
+              role="password"
+              aria-label="password"
+              /* value={values.password}
+                onChange={handleChange} */
+            />
+          </div>
+        </div>
+        <button onClick={submitForm} style={{padding: '10px 20px', border: '1px solid gray'}}>
+          Sign In
+        </button>
+        <button>Cancel </button>
+      </Form>
+    </div>
   );
 };
