727497fffdfc5d61b778dff40caf00c037e7b218
Hassan <h.souiti@gmail.com>
2022-09-28 17:52:06 +0100

    clean the code

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,9 +1,7 @@
 import {LoginForm} from '@features/auth';
-
 export const Login = () => {
   return (
     <>
-      <div style={{margin: '50px 0', textAlign: 'center'}}>login Page</div>
       <LoginForm />
     </>
   );
