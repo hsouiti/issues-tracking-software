951925271f96b700fc941d6fd2729c00963dafab
Hassan <h.souiti@gmail.com>
2022-10-12 08:23:18 +0100

    jest config

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -1,6 +1,18 @@
-module.exports = {
+/* module.exports = {
   preset: 'ts-jest',
   //testEnvironment: 'jest-environment-jsdom',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
+ */
+
+import type {Config} from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  //testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
+
+export default config;
