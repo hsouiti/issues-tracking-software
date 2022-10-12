b73831f01eb1d0d6f5d299252b19a219012f0416
Hassan <h.souiti@gmail.com>
2022-10-12 15:04:03 +0100

    loading component

--- client/src/components/loading.tsx
+++ client/src/components/loading.tsx
@@ -1,3 +1,8 @@
+import loading from '../assets/loading.gif';
 export const Loading = () => {
-  return <p>loading...</p>;
+  return (
+    <div className="w-screen h-screen  flex justify-center items-center">
+      <img className="w-[200px] h-[200px]" src={loading} alt="Laoding..." />
+    </div>
+  );
 };
