5db1a6d7a3f7802d95fdaaf5dfe66e6fa8388b5b
Hassan <h.souiti@gmail.com>
2022-10-19 22:08:14 +0100

    config css import for jest

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -1,11 +1,3 @@
-/* module.exports = {
-  preset: 'ts-jest',
-  //testEnvironment: 'jest-environment-jsdom',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-};
- */
-
 import type {Config} from 'jest';
 
 const config: Config = {
@@ -13,6 +5,11 @@ const config: Config = {
   //testEnvironment: 'jest-environment-jsdom',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
 };
 
 export default config;
