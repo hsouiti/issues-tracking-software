3366c6a67d02d3190315514b5dc5cfb12e79e6cc
Hassan <h.souiti@gmail.com>
2022-10-19 13:33:13 +0100

    add Toast react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,18 @@ import {lazy, Suspense} from 'react';
 import '@styles/index.css';
 import {Loading} from './components/loading';
 import {BrowserRouter} from 'react-router-dom';
+import {ToastContainer} from 'react-toastify';
 //const Router = lazy(() => import('./routes'));
 const Router = lazy(() => import('./routes/router'));
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <>
+      <ToastContainer />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </>
   );
 };
 
