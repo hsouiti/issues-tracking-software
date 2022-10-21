e760c76f525cc543383fdb80db997bde9e59b61c
Hassan <h.souiti@gmail.com>
2022-10-21 10:15:57 +0100

    test InputField

--- client/src/components/InputField/index.spec.tsx
+++ client/src/components/InputField/index.spec.tsx
@@ -1,12 +1,34 @@
-import {render, screen} from '@testing-library/react';
+import {render, screen, fireEvent} from '@testing-library/react';
 import userEvent from '@testing-library/user-event/';
-import TextField from '.';
+import InputField from './index';
 
 const user = userEvent.setup();
 
-describe('Testing the TxetField Component', () => {
-  render(<TextField value="test" />);
-  it('first test', () => {
-    console.log('test goes here');
+const baseProps = {
+  name: 'mockName',
+  value: '',
+  label: 'mockLabel',
+  'data-testid': 'mockInput',
+  onChange: jest.fn(),
+  error: 'something went wrong',
+};
+const setupInput = (props: any) => {
+  render(<InputField {...props} />);
+  const input = screen.getByTestId('mockInput') as HTMLInputElement;
+  return {input};
+};
+
+describe('Testing the InputField Component', () => {
+  it('should render', () => {
+    const {input} = setupInput(baseProps);
+    expect(input).toBeInTheDocument();
+    expect(screen.getByText('mockLabel')).toBeInTheDocument();
+    expect(screen.getByRole('mockName-message')).toBeInTheDocument();
+  });
+
+  it('should change value', async () => {
+    const newProps = {...baseProps, value: 'mockValue'};
+    const {input} = setupInput(newProps);
+    expect(input.value).toBe('mockValue');
   });
 });
--- client/src/components/InputField/index.tsx
+++ client/src/components/InputField/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {BiCheck} from 'react-icons/bi';
 
 interface TextFieldProps {
   name?: string;
@@ -7,15 +6,17 @@ interface TextFieldProps {
   value: string;
   disabled?: boolean;
   placeholder?: string;
+  id?: string;
   label?: string;
   rule?: string;
-  onChange?: (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  onChange?: (event: React.ChangeEvent<InputElement>) => void;
   styles?: React.CSSProperties;
   error?: string;
   role?: string;
   textarea?: boolean;
-  reference?: HTMLInputElement | HTMLTextAreaElement;
 }
+
+type InputElement = HTMLInputElement | HTMLTextAreaElement;
 /*
 // TODO: 
 // Add border red in error
@@ -23,39 +24,39 @@ interface TextFieldProps {
 // 
 */
 
-export const InputField = ({
+const InputField = ({
   label,
   styles,
   error,
   rule,
   role,
-  textarea,
-  reference,
+  textarea = false,
   ...rest
 }: TextFieldProps) => {
   const InputElement = textarea ? 'textarea' : 'input';
+
   return (
     <div className="form__group" style={styles}>
-      <label htmlFor={rest.name}>
-        <p className="font-medium text-sm text-slate-700 pb-2">{label}</p>
-
-        <InputElement
-          className="w-full py-2 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow placeholder:text-sm"
-          role={role}
-          data-rule={rule}
-          ref={reference}
-          {...rest}
-        />
-
-        {error && (
-          <p
-            role={`${rest.name}-message`}
-            className="w-full px-0 mt-2 text-pink-600 text-sm flex flex-wrap justify-start gap-x-4 gap-y-2"
-          >
-            {error}
-          </p>
-        )}
+      <label htmlFor={rest.name} className="font-medium text-sm text-slate-700 pb-2">
+        {label}
       </label>
+      <InputElement
+        className="w-full py-2 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow placeholder:text-sm"
+        role={role}
+        data-rule={rule}
+        {...rest}
+      />
+
+      {error && (
+        <p
+          role={`${rest.name}-message`}
+          className="w-full px-0 mt-2 text-pink-600 text-sm flex flex-wrap justify-start gap-x-4 gap-y-2"
+        >
+          {error}
+        </p>
+      )}
     </div>
   );
 };
+
+export default InputField;
