b311c9382acb46b94f1dd150740b8271c4b0211e
Hassan <h.souiti@gmail.com>
2023-02-27 08:57:08 +0100

    check if the rror is an Error response

--- client/src/pages/error-page.tsx
+++ client/src/pages/error-page.tsx
@@ -23,6 +23,20 @@ const ErrorPage = () => {
         </button>
       </>
     );
+  } else {
+    return (
+      <>
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <button
+          role="link"
+          onClick={() => navigate('/', {replace: true})}
+          className=" w-[150px] mt-4 py-2 font-medium text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center disabled:opacity-50"
+        >
+          Back
+        </button>
+      </>
+    );
   }
 };
 
