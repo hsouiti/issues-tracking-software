c2de464e6bcdb69468c94fcac80a32f98491fd6e
Hassan <h.souiti@gmail.com>
2023-02-27 08:41:51 +0100

    errorpage

--- client/src/pages/error-page.tsx
+++ client/src/pages/error-page.tsx
@@ -1,5 +1,29 @@
+import {isRouteErrorResponse, useRouteError, useNavigate} from 'react-router-dom';
+
+type errorType = {statusText: string; message: string};
+
 const ErrorPage = () => {
-  return <div>Page not found</div>;
+  const error = useRouteError();
+  const navigate = useNavigate();
+  if (isRouteErrorResponse(error)) {
+    return (
+      <>
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <h2>{error.status}</h2>
+        <p>{error.statusText}</p>
+        {error.data?.message && <p>{error.data.message}</p>}
+
+        <button
+          role="link"
+          onClick={() => navigate('/', {replace: true})}
+          className=" w-[150px] mt-4 py-2 font-medium text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center disabled:opacity-50"
+        >
+          Back
+        </button>
+      </>
+    );
+  }
 };
 
 export default ErrorPage;
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,40 +1,29 @@
 import {lazy, Suspense} from 'react';
-import {BrowserRouter, createBrowserRouter, RouterProvider} from 'react-router-dom';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 
 import '@styles/index.css';
+
+// Loading
+import {Loading} from '@components/loading';
 // pages
-/* 
+
 import Dashboard from '@pages/dashboard';
 import Index from '@pages/index';
 import Users from '@pages/users';
 import Projects from '@pages/projects';
 import Issues from '@pages/issues';
 import ErrorPage from '@pages/error-page';
-import Login from '@pages/login';
 import Register from '@pages/register';
-import RequireAuth from '@pages/requireAuth'; */
-
-import {Loading} from '@components/loading';
-// pages
-const Dashboard = lazy(() => import('@pages/dashboard'));
-const Index = lazy(() => import('@pages/index'));
-const Users = lazy(() => import('@pages/users'));
-const Projects = lazy(() => import('@pages/projects'));
-const Issues = lazy(() => import('@pages/issues'));
-const ErrorPage = lazy(() => import('@pages/error-page'));
-const Register = lazy(() => import('@pages/register'));
-const RequireAuth = lazy(() => import('@pages/requireAuth'));
-const Login = lazy(() => import('@pages/login'));
+import RequireAuth from '@pages/requireAuth';
+import Login from '@pages/login';
 
 const routes = createBrowserRouter([
   {
     path: '/',
     element: (
-      <Suspense fallback={<Loading />}>
-        <RequireAuth>
-          <Index />
-        </RequireAuth>
-      </Suspense>
+      <RequireAuth>
+        <Index />
+      </RequireAuth>
     ),
     errorElement: <ErrorPage />,
     children: [
@@ -43,35 +32,22 @@ const routes = createBrowserRouter([
         children: [
           {
             index: true,
-            element: (
-              <Suspense fallback={<Loading />}>
-                <Dashboard />
-              </Suspense>
-            ),
+            element: <Dashboard />,
           },
           {
             path: 'projects',
-            element: (
-              <Suspense fallback={<Loading />}>
-                <Projects />
-              </Suspense>
-            ),
+            element: <Projects />,
+            /*  loader: (data) => console.log('laoder data ', data), */
           },
           {
             path: 'issues',
-            element: (
-              <Suspense fallback={<Loading />}>
-                <Issues />
-              </Suspense>
-            ),
+            element: <Issues />,
+            loader: (data) => console.log('laoder data ', data),
           },
           {
             path: 'users',
-            element: (
-              <Suspense fallback={<Loading />}>
-                <Users />
-              </Suspense>
-            ),
+            element: <Users />,
+            loader: (data) => console.log('laoder data ', data),
           },
         ],
       },
@@ -79,19 +55,11 @@ const routes = createBrowserRouter([
   },
   {
     path: '/login',
-    element: (
-      <Suspense fallback={<Loading />}>
-        <Login />,
-      </Suspense>
-    ),
+    element: <Login />,
   },
   {
     path: '/register',
-    element: (
-      <Suspense fallback={<Loading />}>
-        <Register />
-      </Suspense>
-    ),
+    element: <Register />,
   },
 ]);
 
